32bd8310d440dc31b46cbdb6768c974515e92f57
JoyceQuerubino <joycequerubino5@gmail.com>
2024-07-27 00:46:37 -0300

    chore: initialize repository with basic structure

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,32 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@contexts': './src/contexts',
+            '@services': './src/services',
+            '@types': './src/types',
+            '@hooks': './src/hooks',
+            '@helpers': './src/helpers',
+            '@theme': './src/theme',
+          },
+        },
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          allowUndefined: false,
+        },
+      ],
+    ],
   };
 };
